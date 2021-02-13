06e65a78ebe8eb89ab33aac7dc1ebbc72c56a96a
Ahmaud R. Templeton <ahmaud@gmail.com>
2021-02-13 09:11:03 -0500

    commit tree 672dbe37a6011ca384173f5d7065d7b3a6dd3c86

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,40 @@
-class BankAccount
+require_relative './transfer.rb'
 
+class BankAccount
+    attr_accessor :status, :balance
+    attr_reader :name
+
+    @@all = []
+
+    def initialize(name)
+        @name = name
+        @balance = 1000
+        @status = "open"
+        @@all << self
+    end
+
+    def deposit(amount)
+        @balance+=amount
+    end
+
+
+    def display_balance
+        "Your balance is $#{@balance}."
+    end
+
+    def valid?
+        if @balance > 0 && @status == "open"
+            true
+        else
+            false
+        end
+    end
+
+    def close_account
+        @status = "closed"
+    end
+
+    def self.all
+        @@all
+    end
 end
--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,37 @@
+require 'pry'
+require_relative './bank_account.rb'
+
 class Transfer
-  # your code here
+  attr_accessor :sender, :receiver, :amount, :status
+  
+  def initialize(sender, receiver, amount)
+    @sender = sender
+    @receiver = receiver
+    @amount = amount
+    @status = "pending"
+  end
+
+  def valid?
+    @receiver.valid? && @sender.valid? #! we set require_relative for banking file, so we can use methods for corresponding objects
+    
+  end
+  # binding.pry
+
+  def execute_transaction
+    # binding.pry
+    if self.status == "pending" && self.valid? == true && @sender.balance > @amount
+      @receiver.deposit(@amount) && @sender.deposit(@amount*-1)
+      self.status = "complete"
+    else
+      @status = "rejected"
+      "Transaction rejected. Please check your account balance."
+    end
+  end
+
+  def reverse_transfer
+    if self.status == "complete"
+      @receiver.deposit(@amount*-1) && @sender.deposit(@amount)
+      self.status = "reversed"
+    end
+  end
 end
